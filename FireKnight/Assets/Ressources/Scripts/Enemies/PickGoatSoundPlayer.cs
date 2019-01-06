d977e595570d0b470e34f12ad4834bf09534e87a
Luca Martinelli <lucaandre.martinelli@ue-germany.de>
2019-01-06 15:44:00 +0100

    Fixed PickGoat steps

--- FireKnight/Assets/Ressources/Scripts/Enemies/PickGoatSoundPlayer.cs
+++ FireKnight/Assets/Ressources/Scripts/Enemies/PickGoatSoundPlayer.cs
@@ -8,13 +8,13 @@ namespace Dim.Enemies
     /////////////////////////////////////////////////
     /// Attached to Pick and Feet of PickGoat, emits sounds on Collision.
     /////////////////////////////////////////////////
-    public class PickGoatSoundPlayer : MonoBehaviour
+    public class PickGoatSoundPlayer : MonoBehaviour, Visualize.IExtraVisualization
     {
 
-        [SerializeField] private float Loudness = 5;
-        [SerializeField] private float Range = 10;
+    //    [SerializeField] private float Loudness = 5;
+    //    [SerializeField] private float Range = 10;
         private float timeStamp;
-        private float cooldown = 0.5f;
+        private float cooldown = 0.1f;
 
         AudioSource source;
 
@@ -31,12 +31,19 @@ namespace Dim.Enemies
             if (other.tag == "Untagged" && Time.time - timeStamp > cooldown)
             {
                 timeStamp = Time.time;
-                SoundMechanicHandler.PlaySound(transform, transform.position, Loudness, Range, false);
+                // SoundMechanicHandler.PlaySound(transform, transform.position, Loudness, Range, false);
                 source.Play();
+                Debug.Log(other.name);
             }
         }
 
-        
+        public string[] GetExtraVisualizationElements()
+        {
+            return new string[]
+            {
+                Mathf.Max(Time.time-timeStamp,0).ToString()
+            };
+        }
     }
 
 }
