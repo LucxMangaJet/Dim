a0d970d8d571433d5a965adcbd5f5e8b87bbf6d6
Luca Martinelli <lucaandre.martinelli@ue-germany.de>
2019-01-08 23:28:22 +0100

    Completed UI in intro

--- FireKnight/Assets/Ressources/Scripts/Other/IntroUIElementController.cs
+++ FireKnight/Assets/Ressources/Scripts/Other/IntroUIElementController.cs
@@ -8,18 +8,51 @@ namespace Dim.Visualize
     {
         [SerializeField] float maxDist=10;
         [SerializeField] AnimationCurve animationCurve;
-
+        [SerializeField] KeyCode keyCode;
         SpriteRenderer sRenderer;
+        Collider sCollider;
+
+        bool faded = false;
 
         void Start()
         {
             sRenderer = GetComponent<SpriteRenderer>();
+            sCollider = GetComponent<Collider>();
         }
 
         
         void Update()
         {
-            DistanceBasedAlpha();
+            if (!faded)
+            {
+                DistanceBasedAlpha();
+                if (sCollider.bounds.Contains(LevelHandler.GetPlayer().position))
+                {
+                    if (Input.GetKeyDown(keyCode))
+                    {
+                        faded = true;
+                        StartCoroutine(EndingFade());
+                    }
+                }
+            }
+        }
+
+        private IEnumerator EndingFade()
+        {
+            float counter = 0;
+            float TIME = 0.5f;
+            float startingAlpha = sRenderer.color.a;
+            Color c = sRenderer.color;
+            while (counter < TIME)
+            {
+                c.a = Mathf.Lerp(startingAlpha, 0, counter / TIME);
+                c.b = Mathf.Lerp(1, 0, counter / TIME);
+                sRenderer.color = c;
+                yield return null;
+                counter += Time.deltaTime;
+            }
+
+            Destroy(gameObject);
         }
 
         private void DistanceBasedAlpha()
