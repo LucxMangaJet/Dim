066cb01c5525ef4eeecee3e8deae1b09b9f8591f
Luca Martinelli <lucaandre.martinelli@ue-germany.de>
2019-01-08 20:41:37 +0100

    Created sound motor

---
+++ FireKnight/Assets/Ressources/Scripts/GameplayElements/SoundMotor.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dim.Interaction {
+
+    /////////////////////////////////////////////////
+    /// Used to toggle Sounds based on Storage Energy Level
+    /////////////////////////////////////////////////
+    public class SoundMotor : InteractionBase
+    {
+
+        [SerializeField] byte activationEnergy;
+        [SerializeField] bool exactEnergy;
+
+        [SerializeField] AudioSource audioSource;
+        bool active;
+
+
+        public override void OnEnergyChange(byte newEnergy)
+        {
+            bool query = newEnergy >= activationEnergy;
+            if (exactEnergy)
+            {
+                query = newEnergy == activationEnergy;
+            }
+
+            if (query)
+            {
+                if (!active)
+                {
+                    active = true;
+                    audioSource.Play();
+                }
+            }
+            else
+            {
+                if (active)
+                {
+
+                    audioSource.Stop();
+                
+                }
+            }
+        }
+
+    }
+}
+
