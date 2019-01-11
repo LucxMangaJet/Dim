44778e9bd9eefca676ebd4ff173a85921d9c9e9b
Luca Martinelli <lucaandre.martinelli@ue-germany.de>
2019-01-11 18:47:01 +0100

    Added transition from ending to end scene

---
+++ FireKnight/Assets/Ressources/Scripts/GameplayElements/EndSceneMotor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Dim.Interaction
+{
+    public class EndSceneMotor : InteractionBase
+    {
+
+        [SerializeField] byte activationEnergy;
+
+        [SerializeField] float timeToToggle;
+        [SerializeField] int sceneToLoadIndex;
+
+        [SerializeField] Image image;
+
+
+        public override void OnEnergyChange(byte newEnergy)
+        {
+            Debug.Log("AAA");
+            if (newEnergy == activationEnergy)
+            {
+                Debug.Log("V");
+                StartCoroutine(endScene());
+
+            }      
+        }
+
+
+        private IEnumerator endScene()
+        {
+
+            float counter = 0;
+
+            while (counter < timeToToggle)
+            {
+                image.color = Color.Lerp( new Color(1,1,1,0),Color.white, counter / timeToToggle);
+
+                yield return null;
+                counter += Time.deltaTime;
+            }
+
+            LevelHandler.ShouldLoadFromSaveFile(null);
+            GlobalMethods.LoadSceneAndSaveProgress(sceneToLoadIndex);
+
+        }
+
+    }
+}
