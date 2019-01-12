6c9a4c48d5ee5fcef3db1d084c0e68771a482d3f
Luca Martinelli <lucaandre.martinelli@ue-germany.de>
2019-01-12 15:34:09 +0100

    Moved objects that did not belong in Assets/ or Assets/Scenes/

---
+++ FireKnight/Assets/Ressources/Scripts/Other/Lighttrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lighttrigger : MonoBehaviour {
+
+
+
+    [SerializeField] GameObject Objecttotigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Objecttotigger.SetActive(true);
+    }
+
+
+
+
+
+
+    // Use this for initialization
+    void Start ()
+
+    {
+		
+	}
+	
+	
+}
